84a1b2bdea736ebf7878c8ea1074b668f1e9c1c4
Wilson Carvalho <wilson@fuerzastudio.com.br>
2021-11-19 15:32:22 -0300

    style: flexbox organization init

---
+++ pages/Home/index.tsx
@@ -0,0 +1,168 @@
+import Image from "next/image";
+
+import InstagramIcon from "../../public/assets/instagram-icon.svg";
+import TwitterIcon from "../../public/assets/twitter-icon.svg";
+import FacebookIcon from "../../public/assets/facebook-icon.svg";
+import GlobalIcon from "../../public/assets/global-icon.svg";
+
+import {
+  Container,
+  RowContainer,
+  RowList,
+  CommonListContainer,
+  ListItem,
+  MainLinksContainer,
+  RowContainerEnd,
+} from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <MainLinksContainer>
+        <section>
+          <div>
+            <h3>Atendimento</h3>
+          </div>
+          <CommonListContainer>
+            <ListItem>
+              <a href="/">Central de Ajuda</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Informações de segurança</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Opções de cancelamento</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Nossa resposta à pandemia de COVID-19</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Apoie pessoas com deficiência</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Denuncie um problema do bairro</a>
+            </ListItem>
+          </CommonListContainer>
+        </section>
+
+        <section>
+          <div>
+            <h3>Comunidade</h3>
+          </div>
+          <CommonListContainer>
+            <ListItem>
+              <a href="/">Airbnb.org: ajuda em desastres</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Apoie refugiados afegãos</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Diversidade e pertencimento</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Combate à discriminação</a>
+            </ListItem>
+          </CommonListContainer>
+        </section>
+
+        <section>
+          <div>
+            <h3>Hospedagem</h3>
+          </div>
+          <CommonListContainer>
+            <ListItem>
+              <a href="/">Experimente hospedar</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">AirCover: proteção ao hospedar</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Explore recursos para hospedar</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Visite o fórum da comunidade</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Hospede de forma responsável</a>
+            </ListItem>
+          </CommonListContainer>
+        </section>
+
+        <section>
+          <div>
+            <h3>Sobre</h3>
+          </div>
+          <CommonListContainer>
+            <ListItem>
+              <a href="/">Newsroom</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Descubra os novos recursos</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Carta dos nossos fundadores</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Carreiras</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Investidores</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Airbnb Luxe</a>
+            </ListItem>
+          </CommonListContainer>
+        </section>
+      </MainLinksContainer>
+
+      <MainLinksContainer>
+        <RowContainer>
+          <span>© 2021 Airbnb, Inc.</span>
+          <div>
+            <span>
+              <span>·</span>
+            </span>
+            <a href="/">Privacidade</a>
+            <span>·</span>
+            <a href="/">Termos</a>
+            <span>·</span>
+            <a href="/">Mapa do site</a>
+            <span>·</span>
+            <a href="/">Informações da empresa</a>
+          </div>
+        </RowContainer>
+
+        <RowContainerEnd>
+          <div>
+            <span>
+              <button type="button">
+                <span>
+                  <Image src={GlobalIcon} width="18" height="18" />
+                </span>
+                <span>Português (BR)</span>
+              </button>
+            </span>
+            <span>
+              <button type="button">
+                <span>R$</span>
+                <span>BRL</span>
+              </button>
+            </span>
+          </div>
+
+          <RowList>
+            <li>
+              <Image src={FacebookIcon} width="18" height="18" />
+            </li>
+            <li>
+              <Image src={InstagramIcon} width="18" height="18" />
+            </li>
+            <li>
+              <Image src={TwitterIcon} width="18" height="18" />
+            </li>
+          </RowList>
+        </RowContainerEnd>
+      </MainLinksContainer>
+    </Container>
+  );
+}
---
+++ pages/Home/styles.tsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  flex: 1;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RowContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+`;
+
+export const RowContainerEnd = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const CommonListContainer = styled.ul`
+  list-style-type: none;
+
+  margin: 0px;
+  padding: 0px;
+`;
+
+export const RowList = styled.ul`
+  list-style-type: none;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const MainLinksContainer = styled.div`
+  width: 100%;
+  max-width: 1600px;
+
+  display: flex;
+  justify-content: space-between;
+
+  padding: 24px 0px;
+`;
+
+export const ListItem = styled.li`
+  margin-top: 16px;
+`;
